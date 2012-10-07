f4451f173d78af905e5c7f7de2f07ff8a735b3ca
Sean Camden <sean@seancamden.com>
2012-10-07 14:44:02 -0700

    Added concatenation commands to bash script

--- fetch-compare-write.sh
+++ fetch-compare-write.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 curl "https://api.twitter.com/1/statuses/user_timeline.xml?include_rts=true&screen_name=wagonlips&count=5" > /tmp/tweets.xml
 diff latest_tweets.xml /tmp/tweets.xml
-if [ $? -eq 1 ]
-  then cp /tmp/tweets.xml latest_tweets.xml
+if [ $? -eq 1 ] 
+  then 
+  cp /tmp/tweets.xml latest_tweets.xml
   python element-tweet-parser.py > /tmp/tweets.html
   cat twitter-full.html >> /tmp/tweets.html
   cp /tmp/tweets.html twitter-full.html
+  cat index-head.html > index.html
+  cat twitter-full.html >> index.html
+  cat index-tail.html >> index.html
 fi
