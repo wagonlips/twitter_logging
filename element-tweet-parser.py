2a399ba24aa3de9bbb9ccf272d75f40c2944205d
Sean Camden <sean@seancamden.com>
2012-10-06 17:07:26 -0700

    Minor edits, added TODO

--- element-tweet-parser.py
+++ element-tweet-parser.py
@@ -2,16 +2,17 @@
 from datetime import datetime
 from lxml import etree
 myXmlFile = etree.parse("latest_tweets.xml")
-myDoc = myXmlFile.xpath('//text')
+myText = myXmlFile.xpath('//text')
 myDate = myXmlFile.xpath('//status/created_at')
 textList = []
 dateList = []
-for x in myDoc:
+for x in myText:
   textList.append(x.text)
 for y in myDate:
   y.text = y.text[4:]
   y.text = y.text.replace("+0000 ","")
   myDate = datetime.strptime(y.text, "%b %d %H:%M:%S %Y") 
   dateList.append(myDate)
+#TODO replace the hard-coded username with a variable
 for a, b in zip(dateList, textList):
   print a, "&lt;wagonlips&gt;", b
