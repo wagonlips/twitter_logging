7eca52778fefd7809d4c4deebc8d923ab17108d7
Sean Camden <sean@seancamden.com>
2012-10-06 23:22:08 -0700

    This is the bash script that will orchestrate the procedure.

---
+++ fetch-compare-write.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl "https://api.twitter.com/1/statuses/user_timeline.xml?include_rts=true&screen_name=wagonlips&count=5" > /tmp/tweets.xml
+diff latest_tweets.xml /tmp/tweets.xml
+if [ $? -eq 1 ]
+  then cp /tmp/tweets.xml latest_tweets.xml
+  python element-tweet-parser.py > /tmp/tweets.html
+  cat twitter-full.html >> /tmp/tweets.html
+  cp /tmp/tweets.html twitter-full.html
+fi
