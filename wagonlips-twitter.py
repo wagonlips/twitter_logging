baa385b11a4c692b56e9032f6f58b9522ad14954
Sean Camden <sean@seancamden.com>
2015-05-25 17:08:30 -0700

    This file handles the twitter extraction using the python-twitter API.

--- wagonlips-twitter.py
+++ wagonlips-twitter.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python
-#import twitter
-#api = twitter.Api() 
-#statuses = api.GetUserTimeline('ttytter')
-#for s in statuses:
-#  print s.created_at + " " + s.text
-#
 import pytz
 import twitter
-
-from datetime import  datetime
-
-USERNAME = 'wagonlips'
-
-api = twitter.Api()
-
-user = api.GetUser(USERNAME)
+import mysecret
+from datetime import datetime
+api = twitter.Api(mysecret.my_consumer_key,mysecret.my_consumer_secret,mysecret.my_access_token_key,mysecret.my_access_token_secret)
 pst_tz = pytz.timezone('America/Los_Angeles')
-
-statuses = api.GetUserTimeline(USERNAME)
+statuses = api.GetUserTimeline()
 for s in statuses:
-    # get UTC timestamp from seconds since epoch
-    utc_dt = datetime.utcfromtimestamp(s.created_at_in_seconds).replace(tzinfo=pytz.utc)
-    # convert to given timezone
-    pst_dt = pst_tz.normalize(utc_dt.astimezone(pst_tz))
-    print '[' + (pst_dt.strftime('%Y-%m-%d %H:%M:%S')) + '] &lt;wagonlips&gt; ' + s.text
+#    # get UTC timestamp from seconds since epoch
+  utc_dt = datetime.utcfromtimestamp(s.created_at_in_seconds).replace(tzinfo = pytz.utc)
+#    # convert to given timezone
+  pst_dt = pst_tz.normalize(utc_dt.astimezone(pst_tz))
+  print '[' + (pst_dt.strftime('%Y-%m-%d %H:%M:%S')) + '] &lt;wagonlips&gt; ' + s.text
