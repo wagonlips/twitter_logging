3f068a04f8398a1e8698182486577573f23017b1
Sean Camden <sean@seancamden.com>
2015-05-25 17:04:18 -0700

    This is the main file that executes the various sub scripts.

--- fetch-compare-write.sh
+++ fetch-compare-write.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-curl "https://api.twitter.com/1/statuses/user_timeline.xml?include_rts=true&screen_name=wagonlips&count=5" > /tmp/tweets.xml
-diff /twitter_logging/latest_tweets.xml /tmp/tweets.xml
+RDIR=~/twitter_logging # the root directory
+$RDIR/wagonlips-twitter.py > $RDIR/latest_tweets.html
+diff $RDIR/latest_tweets.html $RDIR/tweets.html
 if [ $? -eq 1 ] 
   then 
-  cp /tmp/tweets.xml /twitter_logging/latest_tweets.xml
-  python /twitter_logging/element-tweet-parser.py > /tmp/tweets.html
-  cat /twitter_logging/twitter-full.html >> /tmp/tweets.html
-  cp /tmp/tweets.html /twitter_logging/twitter-full.html
-  cat /twitter_logging/index-head.html > /twitter_logging/index.html
-  cat /twitter_logging/twitter-full.html | sort -r | uniq >> /twitter_logging/index.html
-  cat /twitter_logging/index-tail.html >> /twitter_logging/index.html
-  lftp -f /twitter_logging/wagonlips-index-upload.lftp
+  cp $RDIR/latest_tweets.html $RDIR/tweets.html
+  cat $RDIR/tweets.html >> $RDIR/tweets-full.html
+  cat $RDIR/tweets-full.html | sort -r | uniq > $RDIR/index-body.html
+  (for r in $RDIR/index-head.html $RDIR/index-body.html $RDIR/index-tail.html;do cat $r >> $RDIR/index.html;done) > $RDIR/index.html
+lftp -f $RDIR/wagonlips-index-upload.lftp
 fi
