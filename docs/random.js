32d0819bd260bfae7cf5e4676c0f9d33e60ffc15
Sean Camden <sean@seancamden.com>
2016-10-01 15:47:29 -0700

    Adding the styles and the random message thingy.

---
+++ docs/random.js
@@ -0,0 +1,113 @@
+<!-- hide script from old browsers
+
+var liners = new Array(69);
+	liners[0] = "I am right and worthy, but you are wrong and condemned.";
+	liners[1] = "I can't believe I said that.";
+	liners[2] = "Habilitation";
+	liners[3] = "Master of Ignorance";
+	liners[4] = "I wonder how I'm going to deal with this.";
+	liners[5] = "Remember That Time";
+	liners[6] = "It's the toilet.";
+	liners[7] = "Public Display of Infection";
+	liners[8] = "I'll Hold It For You";
+	liners[9] = "Post Coital Embrace";
+	liners[10] = "An indelicate, sour, gloomy, ferocious medley of pedantry and lewdness.";
+	liners[11] = "Something To Tell The Savages";
+	liners[12] = "Irresistable Intellectual Lubricant";
+	liners[13] = "Licentious Modulations";
+	liners[14] = "Mountainous Collections of Cacophonies";
+	liners[15] = "Unlawful Harmonies";
+	liners[16] = "The Chains of Liberty";
+	liners[17] = "The Altruistic Misanthrope";
+	liners[18] = "Nullius in Verba";
+	liners[19] = "Meat pie";
+	liners[20] = "The Boiling point";
+	liners[21] = "How much is they worth?";
+	liners[22] = "Ejaculatory exegesis";
+	liners[23] = "I flush twice because I care.";
+	liners[24] = "Was Jesus a fascist?"
+	liners[25] = "Visualize the end of the petro-chemical era.";
+	liners[26] = "Either the petro-chemicals go, or we do.";
+	liners[27] = "Help end the petro-chemical era.";
+	liners[28] = "Catholic facism is a real threat.";
+	liners[29] = "Baby, baby, baby, baby, baby, baby, baby...";
+	liners[30] = "Perhaps you have not properly educated yourself.";
+	liners[31] = "George W sez dumb is the new smart.";
+	liners[32] = "Our petroleum addiction is a problem.";
+	liners[33] = "Most people are stupid and prefer to stay that way.";
+	liners[34] = "Becoming more wealthy can be detrimental.";
+	liners[35] = "Tax religion.";
+	liners[36] = "This isn't anything at all like I thought it was going to be.";
+	liners[37] = "Let's be selfish together.";
+	liners[38] = "No Respect for Fantastic Beliefs";
+	liners[39] = "What can be asserted without evidence can be dismissed without evidence.";
+	liners[40] = "It's not a war it's a mistake.";
+	liners[41] = "Abolish lobbying.";
+	liners[42] = "Stop! Hammer time.";
+	liners[43] = "This is my life. Stop writing the chapters.";
+	liners[44] = "All theists are equal, they all plan a sequel.";
+	liners[45] = "There will be no peace until there are no gods.";
+	liners[46] = "Theists are inherently violent and incapable of moral action.";
+	liners[47] = "Give up your gods.";
+	liners[48] = "You only need religion because you suck.";
+	liners[49] = "No god, no suicide bombers.";
+	liners[50] = "Agent of Satan";
+	liners[51] = "Creationism is stupid.";
+	liners[52] = "Vexatious Clause Agreement";
+	liners[53] = "War is for pussies.";
+	liners[54] = "Coal=cancer, pollution, shame.";
+	liners[55] = "Harmless Images";
+	liners[56] = "Erroneous Flopwind";
+	liners[57] = "I feel pretty.";
+	liners[58] = "Jesus owes me a porkchop.";
+	liners[59] = "I like the fudge.";
+	liners[60] = "I am your walrus.";
+	liners[61] = "I am not wearing pants.";
+	liners[62] = "I cut the cheese.";
+	liners[63] = "My bologna has a middle name.";
+	liners[64] = "Just another innovative, human-centered, relativistic consumer forming a nuclear family.";
+	liners[65] = "I only sound crazy because you haven't done enough research.";
+	liners[66] = "You can't fight a war against a concept.";
+	liners[67] = "In between right and wrong is a meadow. Meet me there.";
+	liners[68] = "I have sweaty feet.";
+	liners[68] = "The only difference between a cult and a religion: wherewithal.";
+	
+	
+var paras = new Array(25);
+	paras[0] = "I am right and worthy, but you are wrong and condemned.";
+	paras[1] = "Christian faith is supposed to be a scandal to the powers of this world, not an ideology that legitimates them.<br><i>Terry Eagleton</i>";
+	paras[2] = "The result was a military whose purpose was more in being than in being used.";
+	paras[3] = "Never forget: we have only been here a short while; we'll be lucky if we get to stay for long.";
+	paras[4] = "The Foreign Intelligence Surveillance Act court which approves terrorism-related requests for wiretaps, can be summoned over the telephone in a matter of minutes. In its entire history, it has said no to a request only a handful of times, and those were cases where there was a mistake in the request.<br><i>Edward N. Luttwak</i>";
+	paras[5] = "Bush's move was unnecessary if the object of the exercise was to engage in surveillance. It was very useful indeed if the object is to expand executive power.<br><i>Andrew J. Bacevich</i>";
+	paras[6] = "Evil when we are in its power is not felt as evil but as a necessity, or even a duty.<br><i>Simon Weil</i>";
+	paras[7] = "The irony is that the more energetically we pursue our individual, socially isolated right to 'life, liberty and the pursuit of happiness,' the deader the social and natural worlds become.<br><i>Curtis White</i>";
+	paras[8] = "It comes to this: that decision will seem most just which preserves the system of justice even if the system is itself routinely unjust.<br><i>Curtis White</i>";
+	paras[9] = "I do not feel obliged to believe that that same God who has endowed us with sense, reason, and intellect has intended us to forgo their use.<br><i>Galileo Galilei</i>";
+	paras[10] = "More terrifying than the square-jawed, clean-cut barkers with their shoe-shine eyeballs and their carnuba wax smiles hawking their wares on every form of media from morning 'til night and on through to the morning again, non-stop, every single day was the horrific silence that roared in to fill the vacuum that their sudden disappearance created.";
+	paras[11] = "The lawless, the disobedient, the ungodly, the sinners, the unholy, the profane, the murderers, the whoremongers, those that defile themselves, the manstealers, the liars, the purjurers, and any other thing contrary to sound doctrine, you shall inherit the earth.";
+	paras[12] = "And then one day, one of the little creatures became self-aware. And from that point forward, an avalanche of non-sense filled its thoughts.";
+	paras[13] = "The Middle East doesn't need  democracy. The Middle East needs atheism.";
+	paras[14] = "I remember the Iraq War. I was so impressed by how quickly it was won.<br><b><i>Shock and Awe!</i></b><br>And the way democracy bloomed like a great big flower, not just there in Iraq, but all over the Middle East. <b><i>Mission Accomplished!</i></b><br>Yeah, that was awsome.";
+	paras[15] = "I don't have to know an answer. I don't feel frightened by not knowing things, by being lost in a mysterious universe without any purpose, which is the way it really is as far as I can tell. It doesn't frighten me.<br><i>Richard Feynman</i>";
+	paras[16] = "Faith is like a rhinoceros: it won't do much work for you, but at close quarters it will make spectacular claims upon your attention.<br><i>Sam Harris--The End of Faith</i>";
+	paras[17] = "What can be asserted without evidence, can be dismissed without evidence.<br><i>Christopher Hitchens</i>";
+	paras[18] = "Remember the War on Drugs? And how there are no more drugs now? Yeah, that was awsome! The War on Terror's gonna' be just as cool!";
+	paras[19] = "Problems With Theism:<br>The god concept<br>The eternity concept<br>The irrational elevation of homo-sapiens to the detriment of all other species<br>Conclusions reached without evidence";
+	paras[20] = "Things I Believe:<br>I believe in the equality of opposites.<br>I believe in having faith in people.<br>I believe hate is a product of recognizing one's self in others.";
+	paras[21] = "Even war, in the conventional sense,  seems like a tired and outdated concept that a truly enlightened people would have no use for. But then, just when it seems we are becoming more enlightened, along comes some numbskull who desperately needs the tar kicked out of him.";
+	paras[22] = "Do you know what Janet did to Davey?<br>Do you know what Henry did to Timmy?<br>Do you know what August did to Sally?<br>Do you know why Manuel hates the music?<br>Do you know what Georgey did for daddy?<br>Do you know why Saddam had to go?";
+	paras[23] = "The trouble this administration has caused will be around long after the money it delivered to its friends through tax cuts and lucrative government contracts has been spent.";
+	paras[24] = "All I really wanted was for my biological parents to be in love and still married.<br><i>Katherine Tarbox</i>";
+	
+	
+function rand(n) {
+	seed = (0x015a4e35 * seed) % 0x7fffffff;
+	return (seed >> 16) % n;
+	}
+
+	var now = new Date()
+	var seed = now.getTime() % 0xffffffff;
+	
+
+// end hiding script from old browsers -->	
