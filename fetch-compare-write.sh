024ec7917b6c10d8e47bf4aec583543be153a112
Sean Camden <sean@seancamden.com>
2017-05-29 15:10:59 -0700

    Removed reference to Genius cuz just ain't using it anymore. Removed Sam Harris quote, replaced with one from Oscar Wilde.

--- fetch-compare-write.sh
+++ fetch-compare-write.sh
@@ -8,5 +8,6 @@ if [ $? -eq 1 ]
   cat $RDIR/tweets.html >> $RDIR/tweets-full.html
   cat $RDIR/tweets-full.html | sort -r | uniq > $RDIR/index-body.html
   (for r in $RDIR/index-head.html $RDIR/index-body.html $RDIR/index-tail.html;do cat $r >> $RDIR/index.html;done) > $RDIR/index.html
-lftp -f $RDIR/wagonlips-index-upload.lftp
+# lftp -f $RDIR/wagonlips-index-upload.lftp
+scp -i ~/.ec2/ami-wagon-key-pair.pem $RDIR/index.html ec2-user@ec2-52-8-132-77.us-west-1.compute.amazonaws.com:site-wagonlips
 fi
